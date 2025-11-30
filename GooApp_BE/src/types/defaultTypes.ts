91dd6964ddbe47b55930b013e5f6e17a234b2ed1
itisMilob <itismilob@gmail.com>
2025-12-01 00:51:05 +0900

    refactor: backend 레포 분리

--- GooApp_BE/src/types/defaultTypes.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response, NextFunction } from 'express';
-
-// 컨트롤러의 프롭 타입을 공통적으로 사용하기 위해 선언
-export type Controller = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => void;
